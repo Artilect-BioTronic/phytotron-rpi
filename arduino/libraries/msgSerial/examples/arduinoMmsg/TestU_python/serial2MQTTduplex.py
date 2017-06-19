2eb60613c9c41830a934d95cf09aa24ef880f4a6
CrazyFraug <bozec.arnaud@free.fr>
2017-06-19 18:28:04 +0200

    change attribute topic as dict
    it more complex to write but more flexible

--- arduino/libraries/msgSerial/examples/arduinoMmsg/TestU_python/serial2MQTTduplex.py
+++ arduino/libraries/msgSerial/examples/arduinoMmsg/TestU_python/serial2MQTTduplex.py
@@ -6,40 +6,32 @@ import paho.mqtt.publish as publish
 import serial
 import time
 import ast
-import sys, getopt
+import os, sys, getopt
 
 # IP address of MQTT broker
 # example of free MQTT broker:  'iot.eclipse.org'
 hostMQTT='localhost'
 portMQTT=1883
 filePassMqtt="./passMqtt.txt"
-
 clientId='myNameOfClient'
 
 logfile=sys.stdout
-logStartTime=0.
 
 devSerial='/dev/ttyUSB0'   # serial port the arduino is connected to
+# I often use 9600, because I had many pb with pyserial at 38400 !!!
 baudRate=115200
 
-mqTopic1='/domotique/garage/topictest/'
-mqTopic2='/domotique/garage/topictest2/'
+# we convert  mqtt topic  to prefix for arduino
+prefFromTopic = {'phytotron/oh/':'SD+', 'phytotron/sys/':'AT+'}
+# arduino responds with those prefixes, we convert in topics
+topicFromPref = {'SD+':'phytotron/py/', 'AT+':'phytotron/py/'}
 
-# serial msg to arduino begin  with  prefTopic2 / prefTopic1 and end with endOfLine
-prefTopic1='CM+'
-prefTopic2='AT+'
+# serial msg to arduino begin  with  prefix  and end with endOfLine
+# for instance:  AT+ls\n
 endOfLine='\n'
-# arduino responds with those 2 kind of messages
-msg2py='2py'
-msg2mqtt='2mq'
 
 sleepBetweenLoop=1    # sleep time (eg: 1s) to slow down loop
-sleepResponse=0.09    # sleep to leave enough time for the arduino to respond immediately
-
-namePy='py0'
-topFromPy= namePy + '/'
-topFromOH='oh/'
-topFromSys='sys/'
+sleepResponse=0.1    # sleep to leave enough time for the arduino to respond immediately
 
 
 # use to sort log messages
@@ -49,38 +41,37 @@ def logp (msg, gravity='trace'):
 # log file must not grow big
 # I need to overwrite it often
 def reOpenLogfile(logfileName):
-	"re open logfile, I do it because it must not grow big"
-	global logStartTime, logfile
-	#
-	if logfileName != '' :
-		try:
-			# I close file if needed
-			if ( not logfile.closed) and (logfile.name != '<stdout>') :
-				logfile.close()
-			# file will be overwritten
-			if (logfileName != '<stdout>') :
-				logfile = open(logfileName, "w", 1)
-			logStartTime = time.time()
-			logp('logStartTime:' + time.asctime(time.localtime(time.time())), 'info')
-		except IOError:
-			print('[error] could not open logfile:' + logfileName)
-			sys.exit(3)
-	else :
-		print('I cant re open logfile. name is empty')
+    "re open logfile, I do it because it must not grow big"
+    global logfile
+    #
+    if logfileName != '' :
+        try:
+            # I close file if needed
+            if ( not logfile.closed) and (logfile.name != '<stdout>') :
+                logfile.close()
+            # file will be overwritten
+            if (logfileName != '<stdout>') :
+                logfile = open(logfileName, "w", 1)
+                logp('logStartTime:' + time.asctime(time.localtime(time.time())), 'info')
+        except IOError:
+            print('[error] could not open logfile:' + logfileName)
+            sys.exit(3)
+    else :
+        print('I cant re open logfile. name is empty')
 
 
 def read_args(argv):
     # optional args have default values above
-    global logfile, hostMQTT, baudRate, namePy, mqTopic1, mqTopic2, devSerial
+    global logfile, hostMQTT, baudRate, prefFromTopic, topicFromPref, devSerial
     logfileName = ''
     try:
-        opts, args = getopt.getopt(argv,"hl:b:r:n:t:u:d:",["logfile=","broker=","baudrate=","namepy=","mqTopic1=","mqTopic2=","devSerial="])
+        opts, args = getopt.getopt(argv,"hl:b:r:n:p:t:d:",["logfile=","broker=","baudrate=","namepy=","prefFromTopic=","topicFromPref=","devSerial="])
     except getopt.GetoptError:
-        print ('serial2MQTTduplex.py -l <logfile> -n <namepy> -b <broker> -r <baudrate> -t <mqTopic1> -u <mqTopic2> -d <devSerial>')
+        print ('serial2MQTTduplex.py -l <logfile> -n <namepy> -b <broker> -r <baudrate> -p <prefFromTopic> -t <topicFromPref> -d <devSerial>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print ('serial2MQTTduplex.py -l <logfile> -n <namepy> -b <broker> -r <baudrate> -t <mqTopic1> -u <mqTopic2> -d <devSerial>')
+            print ('serial2MQTTduplex.py -l <logfile> -n <namepy> -b <broker> -r <baudrate> -p <prefFromTopic> -t <topicFromPref> -d <devSerial>')
             sys.exit()
         elif opt in ("-l", "--logfile"):
             logfileName = arg
@@ -88,27 +79,23 @@ def read_args(argv):
             baudRate = arg
         elif opt in ("-b", "--broker"):
             hostMQTT = arg
-        elif opt in ("-n", "--namepy"):
-            namePy = arg
-        elif opt in ("-t", "--mqTopic1"):
-            mqTopic1 = arg
-        elif opt in ("-u", "--mqTopic2"):
-            mqTopic2 = arg
+        elif opt in ("-p", "--prefFromTopic"):
+            prefFromTopic = ast.literal_eval(arg)
+        elif opt in ("-t", "--topicFromPref"):
+            topicFromPref = ast.literal_eval(arg)
         elif opt in ("-d", "--devSerial"):
             devSerial = arg
     logp('logfile is '+ logfileName, 'debug')
     logp('broker is '+ hostMQTT, 'debug')
     logp('baudrate is '+ str(baudRate), 'debug')
-    logp('namepy is '+ namePy, 'debug')
-    logp('mqTopic1 is '+ mqTopic1, 'debug')
-    logp('mqTopic2 is '+ mqTopic2, 'debug')
+    logp('prefFromTopic is '+ str(prefFromTopic), 'debug')
+    logp('topicFromPref is '+ str(topicFromPref), 'debug')
     logp('devserial is '+ devSerial, 'debug')
     # I try to open logfile
     if logfileName != '' :
         reOpenLogfile(logfileName)
 
 
-logStartTime = time.time()
 if __name__ == "__main__":
 	read_args(sys.argv[1:])
 
@@ -116,13 +103,12 @@ if __name__ == "__main__":
 # if logfile is old, we remove it and overwrite it
 #   because it must not grow big !
 def checkLogfileSize(logfile):
-	"if logfile is old, we remove it and overwrite it because it must not grow big !"
-	global logStartTime
-	if (time.time() - logStartTime) > 600:
-		reOpenLogfile(logfile.name)
+    "if logfile is too big, we remove it and overwrite it because it must not grow big !"
+    if (logfile.name != '<stdout>') and (os.path.getsize(logfile.name) > 900900):
+        reOpenLogfile(logfile.name)
 
 
-def readListSketchFromFile(fileName):
+def readDictPassFromFile(fileName):
     "read filename and append valid dict lines in returned dict"
     logp('reading list of arguments for sketch in file : ' + fileName)
     try:
@@ -133,7 +119,7 @@ def readListSketchFromFile(fileName):
     #
     strLines = fileListSketch.readlines()
     fileListSketch.close()
-    dSketch = {"username":"phytotron", "password":"biotronic"}
+    dSketch = {"username":"user", "password":"pass"}
     for strl in strLines:
         try:
             if (strl[0] != '#'):
@@ -146,18 +132,30 @@ def readListSketchFromFile(fileName):
           logp('line fails as dict:' + strl)
     return dSketch
 
+# check the correspondance  pref <---> topic   are correct
+# check topics end with /
+# topic keys in prefFromTopic must not be in items of topicFromPref
+def checkTopicAndPref(aPrefFromTopic, aTopicFromPref):
+    print('TODO: write  checkTopicAndPref')
+    for itopic in aPrefFromTopic.keys():
+        if type(itopic) != type(''):
+            logp("topic:"+itopic+" must be a string", 'error')
+        if not itopic.endswith('/'):
+            logp("topic:"+itopic+" must end with /", 'error')
+        if aTopicFromPref.has_key(itopic):
+            logp("topic:"+itopic+" can not be in both dict; it would create loop", 'error')
+
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, rc):
     print("Connected to mosquitto with result code "+str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    mqttc.subscribe(mqTopic1+ topFromOH +'#')
-    mqttc.message_callback_add(mqTopic1+ topFromOH+ '#', on_message_mqTopicOH)
-    mqttc.subscribe(mqTopic2+ topFromOH +'#')
-    mqttc.message_callback_add(mqTopic2+ topFromOH+ '#', on_message_mqTopicOH)
-    mqttc.subscribe(mqTopic1+ topFromSys +'#')
-    mqttc.message_callback_add(mqTopic1+ topFromSys+ '#', on_message_mqTopicSys)
+    for itopic in prefFromTopic.keys():
+        if type(itopic) != type(''):
+            continue
+        mqttc.subscribe(itopic +'#')
+        mqttc.message_callback_add(itopic + '#', on_message_mqTopic)
 
 # The callback for when a PUBLISH message is received from the server.
 # usually we dont go to  on_message
@@ -172,31 +170,21 @@ def on_message(client, userdata, msg):
 
 # The callback for when a PUBLISH message is received from the server.
 # usually  we go to a specific callback that we have defined for each topic
-def on_message_mqTopicOH(client, userdata, msg):
-    logp("mqTopicOH:"+msg.topic+" : "+str(msg.payload), 'info')
-    if msg.topic.startswith(mqTopic1) :
-        mqTopic   = mqTopic1
-        prefTopic = prefTopic1
-    else :
-        mqTopic   = mqTopic2
-        prefTopic = prefTopic2
-    cmd = msg.topic.replace(mqTopic, '').replace(topFromOH, '').replace('#','')
-    cmd2arduino = prefTopic + cmd + ':' + str(msg.payload)+ endOfLine
+def on_message_mqTopic(client, userdata, msg):
+    logp("mqTopic:"+msg.topic+" : "+str(msg.payload), 'info')
+    mqTopic = 'notFound'
+    prefTopic = 'noPref'
+    for itopic in prefFromTopic.keys():
+        if msg.topic.startswith(itopic) :
+            mqTopic   = itopic
+            prefTopic = prefFromTopic[itopic]
+    cmd = msg.topic.replace(mqTopic, prefTopic)
+    cmd2arduino = cmd + ':' + str(msg.payload)+ endOfLine
     ser.write(cmd2arduino)
     # I immediately wait a moment for the arduino to respond
     time.sleep(sleepResponse)
     readArduinoAvailableMsg(ser)
 
-# The callback for when a PUBLISH message is received from the server.
-# usually  we go to a specific callback that we have defined for each topic
-def on_message_mqTopicSys(client, userdata, msg):
-    logp("spec callbackSys,"+msg.topic+":"+str(msg.payload), 'info')
-    cmd = msg.topic.replace(mqTopic1, '').replace(topFromSys, '').replace('#','')
-    cmd2arduino = prefTopic2 + cmd + ':' + str(msg.payload)+ endOfLine
-    ser.write(cmd2arduino)
-    # I immediately wait a moment for the arduino to respond
-    time.sleep(sleepResponse)
-    readArduinoAvailableMsg(ser)
 
 # read all available messages from arduino
 def readArduinoAvailableMsg(seri):
@@ -204,44 +192,20 @@ def readArduinoAvailableMsg(seri):
         # because of readline function, dont forget to open with timeout
         response = seri.readline().replace('\n', '')
         #logp ("answer is:" + response, 'debug')
-        if response.startswith(prefTopic1):
-            # prefTopic1: message to send to mqtt
+        mqTopic = 'notFound'
+        prefTopic = 'noPref'
+        for ipref in topicFromPref.keys():
+            if response.startswith(ipref) :
+                mqTopic   = topicFromPref[ipref]
+                prefTopic = ipref
+        #
+        if response.startswith(prefTopic):
+            # prefTopic: message to send to mqtt
             # I dont analyse those messages, I transmit to mqtt
-            tags = response.replace(prefTopic1, '',1).split(':')
+            tags = response.replace(prefTopic, '',1).split(':')
             # security: I strip wildcard in topic before publishing
             topic = tags[0].replace('+', '').replace('#', '')
-            pyTopic = mqTopic1 + topFromPy + topic
-            if len(tags) > 1 :
-                value = ':'.join(tags[1:])
-            else :
-                value = ''
-            # trace
-            logp('{} = {}'.format(pyTopic, value), 'to MQTT')
-            mqttc.publish(pyTopic, value, retain=True)
-        elif response.startswith(prefTopic2):
-            # prefTopic2: message to send to mqtt
-            # I dont analyse those messages, I transmit to mqtt
-            tags = response.replace(prefTopic2, '',1).split(':')
-            # security: I strip wildcard in topic before publishing
-            topic = tags[0].replace('+', '').replace('#', '')
-            pyTopic = mqTopic2 + topFromPy + topic
-            if len(tags) > 1 :
-                value = ':'.join(tags[1:])
-            else :
-                value = ''
-            # trace
-            logp('{} = {}'.format(pyTopic, value), 'to MQTT')
-            mqttc.publish(pyTopic, value, retain=True)
-        elif response.startswith(msg2py):
-            # msg2py: message 2 python only
-            # python use this to check connection with arduino
-            logp ('msg2py '+response.replace(msg2py, '',1), 'info')
-        elif response.startswith(msg2mqtt):
-            # msg2mqtt: message to send to mqtt
-            # I dont analyse those messages, I transmit to mqtt
-            tags = response.replace(msg2mqtt, '',1).split(':')
-            topic = tags[0].replace('+', '').replace('#', '')
-            pyTopic = mqTopic1 + topFromPy + topic
+            pyTopic = mqTopic + topic
             if len(tags) > 1 :
                 value = ':'.join(tags[1:])
             else :
@@ -254,8 +218,10 @@ def readArduinoAvailableMsg(seri):
             logp (response, 'unknown from '+devSerial)
 
 
+# check the correspondance  pref <---> topic   are correct
+checkTopicAndPref(prefFromTopic, topicFromPref)
+
 # connection to arduino
-# I use 9600, because I had many pb with pyserial at 38400 !!!
 ser = serial.Serial(devSerial, baudrate=baudRate, timeout=0.2)
 logp (str(ser), 'info')
 # when we open serial to an arduino, this reset the board; it needs ~3s
@@ -263,7 +229,7 @@ time.sleep(0.2)
 #I empty arduino serial buffer
 response = ser.readline()
 logp ("arduino buffer garbage: " + str(response), 'info')
-time.sleep(3)
+time.sleep(2)
 
 # loop to get connection to arduino
 
@@ -273,7 +239,7 @@ time.sleep(3)
 #---------------------------------------------------
 
 # read password from file  passMqtt.txt
-authInFile = readListSketchFromFile(filePassMqtt)
+authInFile = readDictPassFromFile(filePassMqtt)
 
 # connection to mosquitto
 mqttc = mqtt.Client("", True, None, mqtt.MQTTv31)
